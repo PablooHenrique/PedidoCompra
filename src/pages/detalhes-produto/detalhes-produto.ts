923ce54cac2422ccd8240154c40642184cfdc540
PablooHenrique <pabloohenrique32@gmail.com>
2017-11-05 15:53:31 -0200

    Finalizando a tela de detalhes dos produtos;

--- src/pages/detalhes-produto/detalhes-produto.ts
+++ src/pages/detalhes-produto/detalhes-produto.ts
@@ -1,10 +1,15 @@
-import {Component} from '@angular/core'
+import {Component} from '@angular/core';
+import {ProdutoEntity} from '../../domain/produto/produto-entity';
+import {ProdutoService} from '../../domain/produto/produto-service';
 
 @Component({
     selector: 'detalhes-produto',
     templateUrl: 'detalhes-produto.html'
 })
-
 export class DetalhesProduto{
+    public produto : ProdutoEntity;
 
+    constructor(private _produtoService : ProdutoService){
+        this.produto = this._produtoService.obterUltimoProdutoAdicionadoAoCarrinho();
+    }
 }
